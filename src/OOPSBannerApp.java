89799a9614e586866ba185f99905a7ff9f2b41b1
meenu10802 <meenu10802>
2026-01-30 15:37:24 +0530

    [Meenakshi]: UC2: Render banner format

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,5 +1,29 @@
+/**
+ * OOPS Banner App
+ *
+ * A console-based Java application to demonstrate
+ * Object-Oriented Programming concepts step by step.
+ *
+ * @author YourName
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // UC implementations will be added step by step
+
+        System.out.println(" ********   ********   *********   ******** ");
+        System.out.println(" *      *   *      *   *        *  *        ");
+        System.out.println(" *      *   *      *   *        *  *        ");
+        System.out.println(" *      *   *      *   *********   ******** ");
+        System.out.println(" *      *   *      *   *                  * ");
+        System.out.println(" *      *   *      *   *                  * ");
+        System.out.println(" ********   ********   *           ******** ");
+
     }
 }
